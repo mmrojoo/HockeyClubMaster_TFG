bbd721427a2f4e5e877eca9184fd7ce66c23991f
mario martin <mmartinrojo27@gmail.com>
2024-03-13 18:14:15 +0100

    cambiar foto a medias

--- app/src/main/java/com/example/hockeyclubmaster/ui/AgregarEquipoActivity.java
+++ app/src/main/java/com/example/hockeyclubmaster/ui/AgregarEquipoActivity.java
@@ -1,5 +1,7 @@
 package com.example.hockeyclubmaster.ui;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +9,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,16 +27,20 @@ public class AgregarEquipoActivity extends AppCompatActivity {
     private EditText editTextTeamName;
     private EditText editTextCoachUID;
     private Button buttonAddTeam;
+    private ConstraintLayout backHome;
     private FirebaseFirestore db;
     private RecyclerView recyclerViewTeams;
     private EquiposAdapter equiposAdapter;
     private List<Equipo> equiposList;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_equipo);
 
+
+        backHome = findViewById(R.id.backHome);
         editTextTeamName = findViewById(R.id.editTextTeamName);
         editTextCoachUID = findViewById(R.id.editTextTeamName2);
         buttonAddTeam = findViewById(R.id.buttonAddTeam);
@@ -48,6 +55,13 @@ public class AgregarEquipoActivity extends AppCompatActivity {
         recyclerViewTeams.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewTeams.setAdapter(equiposAdapter);
 
+        backHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AgregarEquipoActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
         buttonAddTeam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/hockeyclubmaster/ui/ProfileActivity.java
+++ app/src/main/java/com/example/hockeyclubmaster/ui/ProfileActivity.java
@@ -1,10 +1,16 @@
 package com.example.hockeyclubmaster.ui;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
@@ -12,17 +18,42 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import com.example.hockeyclubmaster.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
 public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private ConstraintLayout logOut;
+    private ConstraintLayout editProfile;
     private ConstraintLayout backHome;
 
+
+    private ImageView fotoPerfil;
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         mAuth = FirebaseAuth.getInstance();
 
+
+        fotoPerfil = findViewById(R.id.fotoPerfil);
+        editProfile = findViewById(R.id.editProfile);
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        fotoPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
         logOut = findViewById(R.id.logOut);
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +71,58 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+    //Metodo para cerrar sesion
     private void logout() {
         mAuth.signOut();
         Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
+    }
+
+    //Metodo para abrir la galeria
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar imagen"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                Bitmap resizedBitmap = resizeBitmap(bitmap, fotoPerfil.getWidth(), fotoPerfil.getHeight());
+
+                // Aquí puedes establecer la imagen en tu ImageView
+                fotoPerfil.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error al cargar la imagen", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private Bitmap resizeBitmap(Bitmap bitmap, int width, int height) {
+        // Escalar la imagen para que sea del mismo tamaño que el ImageView
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false);
+        // Crear un Bitmap circular
+        Bitmap circularBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // Crear un Canvas con el Bitmap circular
+        android.graphics.Canvas canvas = new android.graphics.Canvas(circularBitmap);
+        // Dibujar un círculo con el Bitmap redimensionado
+        android.graphics.Paint paint = new android.graphics.Paint();
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        canvas.drawCircle(width / 2f, height / 2f, Math.min(width, height) / 2f, paint);
+        // Establecer la opción de pintura de destino
+        paint.setXfermode(new android.graphics.PorterDuffXfermode(android.graphics.PorterDuff.Mode.SRC_IN));
+        // Dibujar el Bitmap redimensionado en el Bitmap circular
+        canvas.drawBitmap(resizedBitmap, 0f, 0f, paint);
+        return circularBitmap;
     }
 }
