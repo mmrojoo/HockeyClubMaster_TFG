6f0e9664044f407fd5644054b53576352c65ae02
mario martin <mmartinrojo27@gmail.com>
2024-03-13 18:15:11 +0100

    Foto de Perfil A medias

---
+++ app/src/main/java/com/example/hockeyclubmaster/ui/EditProfileActivity.java
@@ -0,0 +1,145 @@
+package com.example.hockeyclubmaster.ui;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hockeyclubmaster.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditProfileActivity extends AppCompatActivity {
+    private CircleImageView profileImageView;
+    private Button closeButton, saveButton, changeProfileBtn;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private Uri imageUri;
+    private StorageReference storageProfilePicsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
+        storageProfilePicsRef = FirebaseStorage.getInstance().getReference().child("Profile Pic");
+
+        profileImageView = findViewById(R.id.profile_image);
+        closeButton = findViewById(R.id.btnClose);
+        saveButton = findViewById(R.id.btnSave);
+        changeProfileBtn = findViewById(R.id.change_profile_btn);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditProfileActivity.this, MainActivity.class));
+            }
+        });
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadProfileImage();
+            }
+        });
+
+        changeProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        getUserInfo();
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            profileImageView.setImageURI(imageUri);
+        }
+    }
+
+    private void getUserInfo() {
+        databaseReference.child(mAuth.getCurrentUser().getUid()).child("image").get().addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+            @Override
+            public void onSuccess(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String image = dataSnapshot.getValue().toString();
+                    Picasso.get().load(image).into(profileImageView);
+                }
+            }
+        });
+    }
+
+    private void uploadProfileImage() {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Establece tu perfil");
+        progressDialog.setMessage("Por favor espera, estamos mandando tu data");
+        progressDialog.show();
+
+        if (imageUri != null) {
+            final StorageReference fileRef = storageProfilePicsRef.child(mAuth.getCurrentUser().getUid() + ".jpg");
+            fileRef.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    String downloadUrl = uri.toString();
+                                    databaseReference.child(mAuth.getCurrentUser().getUid()).child("image").setValue(downloadUrl)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(EditProfileActivity.this, "Imagen de perfil actualizada con éxito", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }).addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(EditProfileActivity.this, "Error al actualizar la imagen de perfil", Toast.LENGTH_SHORT).show();
+                                                }
+                                            });
+                                }
+                            });
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(EditProfileActivity.this, "Error al subir la imagen a Firebase Storage", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Imagen no seleccionada", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
